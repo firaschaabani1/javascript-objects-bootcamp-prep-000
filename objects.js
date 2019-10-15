a2f4684057ee4fa7e14a9f95a1eaed51157a0d04
Firas Chaabani <firaschaabani1@gmail.com>
2019-10-15 20:24:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {evanessance, save me}
 function updatePlaylist (playlist, evanessance, save me ) {
-  return playlist.assign ({}, 'artist name', {[song]: 'title'});
+  return playlist.assign ({}, 'evanessance', {[song]: 'save me'});
 }
 function removeFromPlaylist (playlist){
 Delete object, 'artist name'
