133be71ed8f8405a03e19869e6a6d5ec696a138a
Firas Chaabani <firaschaabani1@gmail.com>
2019-10-15 19:46:44 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = {evanessance, save me}
+function updatePlaylist (playlist, evanessance, save me ) {
+  return playlist.assign ({}, 'artist name', {[song]: 'title'});
+}
+function removeFromPlaylist (playlist){
+Delete object, 'artist name'
+}
